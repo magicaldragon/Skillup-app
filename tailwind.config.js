3ee4aead63a1cfe077a4c5ddbfa97f6662997784
magicaldragon <phamvunhatcuong@gmail.com>
2025-07-28 21:27:22 +0700

    Add Tailwind CSS and PostCSS config, update dependencies for styling fix

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./Login.tsx",
+    "./frontend/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
